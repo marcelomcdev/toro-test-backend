2a12c2be71129eab34b98f2f348da29d9489fa99
Marcelo Castro <marcelo.castro@sysmap.com.br>
2022-05-06 23:38:29 -0300

    move LoginRouter to its own file

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,40 +1,6 @@
-class LoginRouter {
-    route(httpRequest){
-        if(!httpRequest || !httpRequest.body) {
-            return HttpResponse.serverError()
-        }
-        const {email, password} = httpRequest.body
-        if(!email){
-            return HttpResponse.badRequest('email')
-        }
-        if(!password){
-            return HttpResponse.badRequest('password')
-        }
-    }
-}
 
-class HttpResponse{
-    static badRequest(paramName) {
-        return{
-            statusCode: 400,
-            body: new MissingParamError(paramName)
-        } 
-    }
-
-    static serverError() {
-        return{
-            statusCode: 500
-        } 
-    }
-
-}
-
-class MissingParamError extends Error {
-    constructor(paramName) {
-        super(`Missing param: ${paramName}`)
-        this.name = 'MissingParamError'
-    }
-}
+const LoginRouter = require('./login-router')
+const MissingParamError = require('../helpers/missing-param-error')
 
 describe('Login router', ()=> {
     test('Should return 400 if no email is provided', ()=>{
@@ -48,7 +14,6 @@ describe('Login router', ()=> {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
-
 
     test('Should return 400 if no password is provided', ()=>{
         const sut = new LoginRouter() //system under test
@@ -68,14 +33,10 @@ describe('Login router', ()=> {
         expect(httpResponse.statusCode).toBe(500)
         
     })
-
-
     test('Should return 500 if has no body', ()=>{
         const sut = new LoginRouter() //system under test
         const httpResponse = sut.route({})
         expect(httpResponse.statusCode).toBe(500)
     })
-
-
 })
 
